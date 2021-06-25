ddc3a1efd14862312aca52f67dac4f8ffa8cce95
crazyAT8 <80292649+crazyAT8@users.noreply.github.com>
2021-06-25 06:19:50 -0500

    push 6/25/21

--- Personal_ePortfolio/javascript/standard.js
+++ Personal_ePortfolio/javascript/standard.js
@@ -1,11 +1,69 @@
-$(document).ready(function(){
-    $("#resume").click(function(){
-        alert("Resume: " + $("#jq_get1").text());
-    });
-});
+//All the Elements needed
+          //use querySelectorAll for multiple way to open a modal- we just have one
+      //open                                        //whats being targeted
+const openResumeModalButtons = document.querySelectorAll('[data-modal-target]')
+const openReferencesModalButtons = document.querySelectorAll('[data-modal-target]')
+      //close
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+      //overlay- to show and hide when needed
+const overlay = document.getElementById('overlay')
 
-$(document).ready(function(){                           //putting an action into exsistance
-    $("#references").click(function(){                  //when the button with the references id is clicked
-        alert("References: " + $("#jq_get2").text());   //content goes to an alert window
-    });                                                 //content starts with a title "References"
-});                                                     //concatinated with the text of the given id
+
+//Event Listeners
+
+openResumeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const modal = document.querySelector(button.dataset.modalTarget)
+      openModal(modal)
+    });
+  });
+
+openReferencesModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const modal = document.querySelector(button.dataset.modalTarget)
+      openModal(modal)
+    });
+  });
+  
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const modal = button.closest('.resumeModal')
+      closeModal(modal)
+    });
+  });
+
+  closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const modal = button.closest('.referencesModal')
+      closeModal(modal)
+    });
+  });
+  
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.referencesModal.active')
+    modals.forEach(modal => {
+      closeModal(modal)
+    });
+  });
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.resumeModal.active')
+    modals.forEach(modal => {
+      closeModal(modal)
+    });
+  });
+
+  
+// Functions
+
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+  };
+  
+  function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+  };
